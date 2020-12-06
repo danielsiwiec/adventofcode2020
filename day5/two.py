d706d63ed3e7d229dd8f36960fb4b57145b1045b
Dan Siwiec <daniel.siwiec@gmail.com>
2020-12-06 12:21:59 -0800

    Day 5

---
+++ day5/two.py
@@ -0,0 +1,22 @@
+def calculate_seat_id(str):
+  row = calculate_row(str[:7])
+  col = calculate_col(str[-3:])
+  return row * 8 + col
+
+def calculate_col(str):
+  binary_list = ['0' if char == 'L' else '1' for char in str ]
+  return int(''.join(binary_list), 2)
+
+def calculate_row(str):
+  binary_list = ['0' if char == 'F' else '1' for char in str ]
+  return int(''.join(binary_list), 2)
+
+with open("input", "r") as fd:
+  seats = fd.read().split('\n')
+
+seat_ids = [calculate_seat_id(seat) for seat in seats]
+seat_ids.sort()
+
+for idx, seat_id in enumerate(seat_ids):
+  if seat_id - seat_ids[idx-1]  > 1:
+    print(seat_id-1)
